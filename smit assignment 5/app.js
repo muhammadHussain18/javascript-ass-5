b385ce8069853bc0b128fe235f596127b989a2ca
Muhammad Hussain <122493431+muhammadHussain18@users.noreply.github.com>
2023-02-11 19:24:44 +0500

    add assignment 5 ( Qno # 1 to 7 )

---
+++ smit assignment 5/app.js
@@ -0,0 +1,42 @@
+// // chapter # 14 - 16
+
+// // Qno# 1
+
+// var names = ["",""]
+// console.log(names)
+
+
+// // Qno# 2
+
+// var studentNames = new Array();
+// console.log(studentNames)
+
+
+// // Qno# 3
+
+// var names = ["ali","bilal","hussain","faizan","hamza"]
+// console.log(names)
+
+
+// // Qno# 4
+
+// var num = [23,24,25,26,27]
+// console.log(num)
+
+
+// // Qno# 5
+
+// var bol = ["$","&","%"]
+// console.log(bol)
+
+// // Qno# 6
+
+// var mixedArray = ["hamza","faizan",22,14,"%","&"]
+// console.log(mixedArray)
+
+
+// // Qno# 7
+
+// var edu = ["SSC","HSC","BCS","BS","BCOM","MS","M phill","PHD"]
+// document.write(edu )
+

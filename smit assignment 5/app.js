0a52b930ea486228ae1f1955856172fd3e68a939
Muhammad Hussain <122493431+muhammadHussain18@users.noreply.github.com>
2023-02-14 02:49:02 +0500

    add more question

--- smit assignment 5/app.js
+++ smit assignment 5/app.js
@@ -40,3 +40,105 @@
 // var edu = ["SSC","HSC","BCS","BS","BCOM","MS","M phill","PHD"]
 // document.write(edu )
 
+
+
+
+// // Qno# 8
+
+
+
+//  // Student names
+// var students = ["Ali", "Bilal", "Hamza"];
+
+// // Scores
+// var scores = [450, 480, 470];
+
+// // Total marks
+// var totalMarks = 500;
+
+
+// var result1 = [scores[0] / totalMarks * 100 ]
+// document.write(students[0] + " " + "scores" + " "+ result1 + "  % " + "</br>" )
+
+// var result2 = [scores[1] / totalMarks * 100 ]
+// document.write(students[1] + " " + "scores" + " "+ result2 + "  % "  + "</br>" )
+
+// var result3 = [scores[2] / totalMarks * 100 ]
+// document.write(students[2] + " " + "scores" + " "+ result3 + "  % " + "</br>" )
+
+
+
+// Qno# 9
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Qno# 10
+
+
+
+
+
+
+
+
+
+
+
+
+// // Qno# 11
+
+// var cities = ["karachi","lahore","swat","Hunza"]
+
+// var selectedCities = []
+
+// selectedCities = cities.slice(2) , (3)
+
+// document.write("selected cities is" + " "+ selectedCities);
+
+
+
+
+// // Qno# 12
+
+// var line = ["This " , "is","my","cat"]
+
+// document.write(line[0] + " " + line[1] + " " + line[2] + " " + line[3])
+
+
+
+
+
+
+
+
+
+
+
+
+// var names = ["ghous","ahmed","hussain","faizan","hamza","ali"]
+// // names[0] = [names[4],names[4] = names[0]][0]
+
+// names[0] = [names[3],names[3]=names[0]][0]
+// // names[1] = [names[4],names[4]=names[1]][0]
+// // names[2] = [names[5]]
+
+// console.log(names)
+
+// var a =[1,2,3,4,5]
+
+// var b = new Array(a)
+// b = b[0]
+// b[]=[b[3], b[3] = b[1]][0]
+// // b[0]
+// // b[1]=b[3]
+// console.log(b)
